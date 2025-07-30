599df5655d8b042bfe39470e6ff67214e59395bb
Lala Hashimova <168121382+lalahashim@users.noreply.github.com>
2025-07-30 20:12:36 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./components/Home"
 
 function App(){
   return (
-    <div className="bg-gray-100 dark:bg-slate-900 flex items-center justify-center">
+    <div className="md:bg-gray-50 md:dark:bg-slate-900 flex items-center justify-center">
       <Home />
     </div>
   )
