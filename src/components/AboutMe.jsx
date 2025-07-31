ce41b67deaa8062154233f6fd012c1a65b4a284a
Lala Hashimova <168121382+lalahashim@users.noreply.github.com>
2025-07-31 09:53:15 +0400

    Add files via upload

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,14 @@
+export default function AboutMe(){
+    return(
+        <div className="flex flex-col-reverse items-center lg:flex-row lg:justify-between text-black dark:text-gray-100">
+            <img src="/my-illustration.png" alt="illustration" className="mt-2 lg:mt-0 md:w-80 rounded-xl" />
+            <div className="flex flex-col gap-1 lg:gap-3 lg:w-[67%]">
+                <h3 className="text-2xl md:text-3xl font-bold text-center">About me</h3>
+                <p className="text-sm md:text-lg">I’m a self-taught Web Developer with a strong focus on Frontend development. I’ve studied Web Development, Python and SQL through Udemy. 
+                    I’m passionate about creating beautiful, responsive, and user-friendly web interfaces. In addition to web development, I have a personal interest in artificial intelligence and plan to study it further in the future.
+                    Open to internship, junior frontend developer roles, and remote opportunities.
+                </p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Projects.jsx
@@ -0,0 +1,27 @@
+import projects from '../data/projects.json'
+
+export default function Projects(){
+    return (
+        <div className='dark:text-gray-100 text-black'>
+            <h3 className="text-2xl md:text-3xl font-bold">Projects</h3>
+            <div className='flex flex-wrap justify-between gap-4 mt-3'>
+                {projects.map((project, index) => (
+                    <div key={index} className='md:w-72 bg-gray-100 dark:bg-slate-900 rounded-lg shadow-md dark:shadow-gray-800'>
+                        <img src={project.photo} alt={project.name} className='object-cover w-full rounded-t-lg' />
+                        <div className='p-2 pb-3 text-center'>
+                            <h4 className='font-bold text-center text-lg'>{project.name}</h4>
+                            <p>{project.description}</p>
+                            <p><span className='font-bold'>Tools:</span> {project.tools.join(', ')}</p>
+                            <p className='mb-2'><span className='font-bold'>Date created:</span> {project.dateCreated}</p>
+                            <a href={project.link} target="_blank" rel="noopener noreferrer"
+                                className='rounded-lg bg-amber-300 dark:bg-pink-700 py-1 px-2 hover:shadow-lg cursor-points dark:cursor-dark-points'
+                            >
+                                View Project
+                            </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
