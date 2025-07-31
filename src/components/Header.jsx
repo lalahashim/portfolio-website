2a53ed14dc9c168b96a6820dc5230e4f31207d19
Lala Hashimova <168121382+lalahashim@users.noreply.github.com>
2025-07-31 14:52:21 +0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,7 +34,7 @@ export default function Header(){
                 {darkMode ? <img src="/sun.png" alt="" className="w-6 md:w-9" /> : <img src="/moon.png" alt="" className="w-6 md:w-8" />}
             </button>
             <h1 className="text-3xl md:text-6xl">Lala Hashimova</h1>
-            <h2 className="text-xl md:text-3xl">frontend developer</h2>
+            <h1 className="text-xl md:text-3xl">frontend developer</h1>
             <a href="/cv.pdf" target="_blank" rel="noopener noreferrer" 
                 className="rounded-lg bg-amber-300 dark:bg-pink-700 hover:shadow-lg dark:hover:shadow-gray-800 cursor-points dark:cursor-dark-points text-lg mt-1 py-0.5 px-2 md:py-1 md:px-2.5 md:mt-3 md:text-xl"
             >
