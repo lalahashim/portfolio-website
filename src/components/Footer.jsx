c65455cf858e7814c61b496a827ef9367cbb048d
Lala Hashimova <168121382+lalahashim@users.noreply.github.com>
2025-08-03 13:02:31 +0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,3 @@
 export default function Footer(){
-    return <p className="text-center text-stone-600 dark:text-gray-300">©2025 Lala Hashim</p>
+    return <p className="text-center text-stone-600 dark:text-gray-300">©{new Date().getFullYear()} Lala Hashim</p>
 }
